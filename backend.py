7599d95f5cb846d4ee76449abd2352511a92e15d
yangciou <me@yangciou.com>
2021-06-16 18:18:14 -0400

    clean

---
+++ backend.py
@@ -0,0 +1,223 @@
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+import pandas as pd
+import datetime
+import os
+
+
+# get the data: https://github.com/nytimes/covid-19-data
+# COVID-19 data published by the NYTimes, updated daily
+CUMULATIVE_DATA = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
+ROLLING_DATA = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/rolling-averages/us-states.csv"
+
+
+# matplotlib styles
+titleFont = {"family":"Georgia",
+             "color":"black",
+             "fontweight":"bold"}
+
+colors = ["#003f5c", "#ffa600"]
+
+def createOuterFolder():
+    if not os.path.isdir("graphs"):
+        os.makedirs("graphs")
+
+def createFolder(chartType):
+    createOuterFolder()
+    folderName = "graphs/" + chartType
+    
+    if not os.path.isdir(folderName):
+        os.makedirs(folderName)
+        
+    return folderName
+    
+def createDF(dataType):
+    # argument ex: 'cumulative' 'rolling' hence .upper()
+    # create df variable name 'cumulative_df' or 'rolling_df'
+    if (dataType + "_df") in globals():
+        print(f"{dataType} df already exists.")
+    else:
+        globals()[f"{dataType}_df"] = pd.read_csv(
+                                        globals()[f"{dataType.upper()}_DATA"])
+        # make all dates into datetime (for graphing)
+        df_variable = globals()[f"{dataType}_df"] 
+        df_variable["date"] = pd.to_datetime(df_variable["date"])      
+        print(f"{dataType} df created.")
+
+def getCumulativeCases(state):
+    createDF("cumulative")
+    cases_df = cumulative_df[cumulative_df["state"] == state][["date", "cases"]]
+    return cases_df.reset_index().drop("index", axis=1) # remove old indexing, use new starting from 0
+
+def getCumulativeDeaths(state):
+    createDF("cumulative")
+    deaths_df = cumulative_df[cumulative_df["state"] == state][["date", "deaths"]]
+    return deaths_df.reset_index().drop("index", axis=1) # remove old indexing, use new starting from 0
+
+def getDailyCases(state):
+    createDF("rolling")
+    cases_df = rolling_df[(rolling_df["state"] == state) & (rolling_df["cases_avg"] >= 0)]\
+                                    [["date", "cases_avg"]]
+    return cases_df.reset_index().drop("index", axis=1) # remove old indexing, use new starting from 0
+
+def getDailyDeaths(state):
+    createDF("rolling")
+    deaths_df = rolling_df[(rolling_df["state"] == state) & (rolling_df["deaths_avg"] >= 0)]\
+                                    [["date", "deaths_avg"]]
+    return deaths_df.reset_index().drop("index", axis=1) # remove old indexing, use new starting from 0
+
+
+def lineChart(dataframe1, dataframe2, states, metric, animated):
+    # make dates start same 
+    if (dataframe1["date"][0] < dataframe2["date"][0]):
+        dataframe1 = dataframe1[dataframe1["date"] >= dataframe2["date"][0]]
+    elif (dataframe1["date"][0] > dataframe2["date"][0]):
+        dataframe2 = dataframe2[dataframe2["date"] >= dataframe1["date"][0]]
+
+    fig = plt.figure()
+    fig.set_figwidth(9)
+    fig.set_figheight(5)
+    plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
+    plt.subplots_adjust(bottom = 0.2, top = 0.9)
+    plt.ylabel(metric)
+    plt.xlabel('Dates')
+    plt.title(metric + " Over Time", fontdict=titleFont)
+
+    dataType = dataframe1.columns[1]
+
+    if animated:
+        dataPoints = len(dataframe1.index)
+
+        def animate(i):
+            if (i % 3 == 0): # every 3 points to generate plot faster
+                # plots the dataset from beginning to i
+                plt.plot(dataframe1["date"][:i], dataframe1[dataType][:i], colors[0])
+                plt.plot(dataframe2["date"][:i], dataframe2[dataType][:i], colors[1])
+                plt.legend(states, loc="upper left")
+                print(i)
+                
+        # save_count has +75 frames to give ending time, if no end time then use +1 (.index)   
+        animator = FuncAnimation(fig, animate, repeat=False, interval=5, save_count=dataPoints + 75)
+        animator.save(f"{createFolder('line_chart')}/{states[0].replace(' ', '')}Vs{states[1].replace(' ', '')}_{metric.replace(' ', '')}.gif", writer=PillowWriter(fps=30))
+        return
+    
+    plt.plot(dataframe1["date"], dataframe1[dataType], colors[0])
+    plt.plot(dataframe2["date"], dataframe2[dataType], colors[1])
+    plt.legend(states, loc="upper left")
+    plt.savefig(f"{createFolder('line_chart')}/{states[0].replace(' ', '')}Vs{states[1].replace(' ', '')}_{metric}.png")
+
+def pieChart(dataframe1, dataframe2, states, metric, animated):
+    # make dates start same 
+    if (dataframe1["date"][0] < dataframe2["date"][0]):
+        dataframe1 = dataframe1[dataframe1["date"] >= dataframe2["date"][0]]
+        dataframe1 = dataframe1.reset_index().drop("index", axis=1)
+    elif (dataframe1["date"][0] > dataframe2["date"][0]):
+        dataframe2 = dataframe2[dataframe2["date"] >= dataframe1["date"][0]]
+        dataframe2 = dataframe2.reset_index().drop("index", axis=1)
+
+    labels = states[0], states[1]
+    explode = (0.01, 0.01) 
+    fig, ax = plt.subplots()
+    
+    if animated:
+        def animate(i):
+            if (i % 3 == 0): # every 3 points to generate plot faster
+                try:
+                    total = int(dataframe1.iloc[:,1][i]) + int(dataframe2.iloc[:,1][i])
+                    def numbering(percent):
+                        val = int(round(percent*total/100.0))
+                        return val
+                    ax.clear()
+                    sizes = [int(dataframe1.iloc[:,1][i]), int(dataframe2.iloc[:,1][i])]
+                    ax.pie(sizes, explode=explode, labels=labels, autopct=numbering, shadow=True, startangle=90)
+                    ax.axis('equal')
+                    ax.set_title(metric + f"\n{dataframe1.iloc[i]['date'].strftime('%B, %Y')}", fontdict=titleFont)
+                except (ValueError, KeyError) as e: # animation continues for 75 more frames to give extra time, so stop it
+                    animator.event_source.stop()
+            
+        animator = FuncAnimation(fig, animate, interval=1, save_count=len(dataframe1) + 75)
+        animator.save(f"{createFolder('pie_chart')}/{states[0].replace(' ', '')}Vs{states[1].replace(' ', '')}_{metric.replace(' ', '')}.gif", writer=PillowWriter(fps=30))
+        return # animation done, stop rest of function
+
+    # later clean this part use variable instead of calling the df multiple times
+    total = int(dataframe1.iloc[:,1][-1:]) + int(dataframe2.iloc[:,1][-1:])
+    def numbering(percent):
+        val = int(round(percent*total/100.0))
+        return val
+    # selects last index
+    sizes = [int(dataframe1.iloc[:,1][-1:]), int(dataframe2.iloc[:,1][-1:])]
+    ax.pie(sizes, explode=explode, labels=labels, autopct=numbering,
+            shadow=True, startangle=90)
+    ax.axis('equal')  
+    ax.set_title(metric + f"\nAs of {dataframe1.iloc[-1]['date'].strftime('%B %d, %Y')}", fontdict=titleFont)
+    plt.savefig(f"{createFolder('pie_chart')}/{states[0].replace(' ', '')}Vs{states[1].replace(' ', '')}_{metric}.png")
+
+def barChart(dataframe1, dataframe2, states, metric, animated):
+    # make dates start same 
+    if (dataframe1["date"][0] < dataframe2["date"][0]):
+        dataframe1 = dataframe1[dataframe1["date"] >= dataframe2["date"][0]]
+        dataframe1 = dataframe1.reset_index().drop("index", axis=1)
+    elif (dataframe1["date"][0] > dataframe2["date"][0]):
+        dataframe2 = dataframe2[dataframe2["date"] >= dataframe1["date"][0]]
+        dataframe2 = dataframe2.reset_index().drop("index", axis=1)
+
+    states = [states[0], states[1]]
+    # convert list into index for position
+    position = range(len(states))
+    
+    fig = plt.figure()
+    fig.set_figwidth(8)
+    fig.set_figheight(5)
+    plt.xlabel("U.S. States")
+    plt.ylabel(metric)
+    plt.xticks(position, states)
+
+    if animated:
+        def animate(i):
+            if (i % 3 == 0): # every 3 points to generate plot faster
+                try:
+                    data = [int(dataframe1.iloc[:,1][i]), int(dataframe2.iloc[:,1][i])]
+                    fig.clear()
+                    plt.bar(position, data, color=colors)
+                    plt.title(metric + f"\n{dataframe1.iloc[i]['date'].strftime('%B, %Y')}", fontdict=titleFont)
+                    plt.xlabel("U.S. States")
+                    plt.ylabel(metric)
+                    plt.xticks(position, states)
+                except (ValueError, KeyError) as e: # animation continues for 75 more frames to give extra time, so stop it
+                    animator.event_source.stop()
+                    
+        animator = FuncAnimation(fig, animate, interval=1, save_count=len(dataframe1) + 75) # save_count=len(dataframe1) + 75
+        animator.save(f"{createFolder('bar_chart')}/{states[0].replace(' ', '')}Vs{states[1].replace(' ', '')}_{metric.replace(' ', '')}.gif", writer=PillowWriter(fps=30))
+        return # animation done, stop rest of function
+    
+    data = [int(dataframe1.iloc[:,1][-1:]), int(dataframe2.iloc[:,1][-1:])]
+    plt.bar(position, data, color=colors)
+    plt.title(metric + f"\nAs of {dataframe1.iloc[-1]['date'].strftime('%B %d, %Y')}", fontdict=titleFont)
+    plt.savefig(f"{createFolder('bar_chart')}/{states[0].replace(' ', '')}Vs{states[1].replace(' ', '')}_{metric}.png")
+
+# corresponds to the index of METRIC_TYPES from gui.py
+metricOptions = {1 : getCumulativeCases,
+                 2 : getDailyCases,
+                 3 : getCumulativeDeaths,
+                 4 : getDailyDeaths}
+    
+# corresponds to the index of CHART_TYPES from gui.py
+inputOptions = {1 : lineChart,
+                2 : pieChart,
+                3 : barChart}
+
+createDF("cumulative")
+
+
+# scientific notation
+# ## https://learnui.design/tools/data-color-picker.html
+# random color (randomintnorep?) and let users choose
+# organize code in classes
+
+
+
+
+
+
+
+
---
+++ gui.py
@@ -0,0 +1,168 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter.messagebox import showerror, showinfo
+from backend import *
+
+def generateGraphBtn():
+    state1Input = state1Combobox.get()
+    state2Input = state2Combobox.get()
+    metricInput = metricCombobox.get()
+    chartInput = chartCombobox.get()
+    animationInput = animationCombobox.get()
+
+    errorMessage = "Be sure to select from the available options -- the following options are incorrect:\n\n"
+    originalErrorLen = len(errorMessage)
+    if (not state1Input) or (state1Input not in STATES): errorMessage += "State 1\n"
+    if (not state2Input) or (state2Input not in STATES): errorMessage += "State 2\n"
+    if (not metricInput) or (metricInput not in METRIC_TYPES): errorMessage += "Metric Type\n"
+    if (not chartInput) or (chartInput not in CHART_TYPES): errorMessage += "Chart Type\n"
+    if (not animationInput) or (animationInput not in ANIMATED_OPTIONS): errorMessage += "Animation Option"
+
+    if (len(errorMessage) > originalErrorLen): # there's an error since message added on
+        showerror("Input Fields Incorrect", errorMessage)
+    else: # no error
+        state1df = metricOptions[METRIC_TYPES.index(metricInput)](state1Input)
+        state2df = metricOptions[METRIC_TYPES.index(metricInput)](state2Input)
+        inputOptions[CHART_TYPES.index(chartInput)](state1df, state2df, [state1Input, state2Input], metricInput, animatedInput(animationInput))
+        showinfo("Graphic Generated", "Your graphic has been sucessfully generated!")
+
+def animatedInput(selection):
+    if ANIMATED_OPTIONS.index(selection) == 1:
+        return True
+    return False
+
+STATES = [""] + sorted(cumulative_df["state"].drop_duplicates().tolist())
+METRIC_TYPES = ["", "Total Cumulative Cases", "Daily New Cases (Rolling Avg.)", "Total Cumulative Deaths", "Daily New Deaths (Rolling Avg.)"] 
+CHART_TYPES = ["", "Line Chart", "Pie Chart", "Bar Chart"]
+ANIMATED_OPTIONS = ["", "Animated (Saved as .gif)", "No Animation (Image as .png)"] 
+
+
+### Tkinter 
+window = Tk()
+window.geometry("820x500")
+window.configure(bg = "#FFFFFF")
+window.title("COVID-19 Data Comparison Visualization")
+icon = PhotoImage(file = "assets/virus_icon.png")
+window.iconphoto(True, icon)
+
+canvas = Canvas(
+    window,
+    bg = "#FFFFFF",
+    height = 500,
+    width = 820,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge")
+canvas.place(x = 0, y = 0)
+
+background_img = PhotoImage(file = f"assets/background.png")
+background = canvas.create_image(
+    410.0, 250.0,
+    image=background_img)
+
+canvas.create_text( # center, bold doesn't work, try different method
+    410.0, 45.0,
+    text = "COVID-19 Data Comparison Visualization",
+    fill = "#30882d",
+    font = ("Georgia", 24, "underline"),
+    justify = "center")
+
+canvas.create_text( 
+    409.0, 103.0,
+    text = "Enter two states and choose from the following options \nto generate a graph. The graph will be automatically \ndownloaded and pop up on a new screen.",
+    fill = "#000000",
+    font = ("Georgia", 14),
+    justify = "center")
+
+canvas.create_text(
+    409.5, 189.0,
+    text = "v.s",
+    fill = "#30882d",
+    font = ("Georgia", 20))
+
+# State 1 Input
+state1Combobox = ttk.Combobox(window, value=STATES)
+state1Combobox.current(0)
+state1Combobox.place(
+    x = 190.0, y = 168,
+    width = 151.0,
+    height = 36)
+
+canvas.create_text(
+    265.5, 154.5,
+    text = "State 1 ",
+    fill = "#000000",
+    font = ("Georgia", 14))
+
+# State 2 Input
+state2Combobox = ttk.Combobox(window, value=STATES)
+state2Combobox.current(0)
+state2Combobox.place(
+    x = 479.0, y = 168,
+    width = 151.0,
+    height = 36)
+
+canvas.create_text(
+    554.5, 154.5,
+    text = "State 2 ",
+    fill = "#000000",
+    font = ("Georgia", 14))
+
+# Metric Type Input
+metricCombobox = ttk.Combobox(window, value=METRIC_TYPES)
+metricCombobox.current(0)
+metricCombobox.place(
+    x = 302.0, y = 242,
+    width = 214.0,
+    height = 33)
+
+canvas.create_text(
+    352, 227.5,
+    text = "Metric Type",
+    fill = "#000000",
+    font = ("Georgia", 14))
+
+# Chart Type Input
+chartCombobox = ttk.Combobox(window, value=CHART_TYPES)
+chartCombobox.current(0)
+chartCombobox.place(
+    x = 302.0, y = 305,
+    width = 214.0,
+    height = 33)
+
+canvas.create_text(
+    347.5, 291.5,
+    text = "Chart Type",
+    fill = "#000000",
+    font = ("Georgia", 14))
+
+# Animation Input
+animationCombobox = ttk.Combobox(window, value=ANIMATED_OPTIONS)
+animationCombobox.current(0)
+animationCombobox.place(
+    x = 302.0, y = 371,
+    width = 214.0,
+    height = 33)
+
+canvas.create_text(
+    347.5, 357.5,
+    text = "Animation",
+    fill = "#000000",
+    font = ("Georgia", 14))
+
+# Generate Graph Btn
+btnImg = PhotoImage(file = f"assets/button.png")
+btn = Button(
+    image = btnImg,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = generateGraphBtn,
+    relief = "flat")
+
+btn.place(
+    x = 316, y = 427,
+    width = 188,
+    height = 48)
+
+window.resizable(False, False)
+window.mainloop()
